4fc491bb1508a47ed2902dbf611c4124306c9051
Ivan Alves E Silva <iasilva@brq.com>
2019-05-30 15:37:47 -0300

    template method

---
+++ src/br/com/ivan/design/patterns/models/impostos/ICPP.java
@@ -0,0 +1,22 @@
+package br.com.ivan.design.patterns.models.impostos;
+
+import br.com.ivan.design.patterns.models.Orcamento;
+
+public class ICPP extends TemplateDeImpostoCondicional{
+
+	@Override
+	public double minimaTaxacao(Orcamento orcamento) {
+		return orcamento.getValor() * 0.05;
+	}
+
+	@Override
+	public double maximaTaxacao(Orcamento orcamento) {
+		return 0;
+	}
+
+	@Override
+	public boolean deveUsarMaximaTaxacao(Orcamento orcamento) {
+		return orcamento.getValor() > 500;
+	}
+
+}
---
+++ src/br/com/ivan/design/patterns/models/impostos/IKCV.java
@@ -0,0 +1,33 @@
+package br.com.ivan.design.patterns.models.impostos;
+
+import br.com.ivan.design.patterns.models.Item;
+import br.com.ivan.design.patterns.models.Orcamento;
+
+public class IKCV extends TemplateDeImpostoCondicional{
+
+	private boolean temItemMaiorQue100ReaisNo(Orcamento orcamento) {
+
+		for(Item item : orcamento.getItens()) {
+			if(item.getValor() > 100) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public double minimaTaxacao(Orcamento orcamento) {
+		return orcamento.getValor() * 0.06;
+	}
+
+	@Override
+	public double maximaTaxacao(Orcamento orcamento) {
+		return orcamento.getValor() * 0.10;
+	}
+
+	@Override
+	public boolean deveUsarMaximaTaxacao(Orcamento orcamento) {
+		return orcamento.getValor() > 500 && temItemMaiorQue100ReaisNo(orcamento);
+	}
+
+}
